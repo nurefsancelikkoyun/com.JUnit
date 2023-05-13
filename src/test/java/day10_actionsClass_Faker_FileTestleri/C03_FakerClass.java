7bb20857d9d139dc76184a053baeeb21c81fe864
Nurefşan <nurrcelikk50@gmail.com>
2023-05-13 15:42:53 +0300

    day06

---
+++ src/test/java/day10_actionsClass_Faker_FileTestleri/C03_FakerClass.java
@@ -0,0 +1,65 @@
+package day10_actionsClass_Faker_FileTestleri;
+
+import com.github.javafaker.Faker;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utilities.TestBase;
+
+public class C03_FakerClass extends TestBase {
+
+     /*
+        Faker kutuphanesi
+        feyk degerler olusturabilecegimiz bir kutuphanedir
+        buradan deger uretebilmek icin
+        faker class'indan bir obje olusturmaliyiz
+     */
+
+    @Test
+    public void test01() throws InterruptedException {
+        //1- https://www.facebook.com adresine gidelim
+        driver.get("https://www.facebook.com");
+        //2- Yeni hesap olustur butonuna basalim
+        driver.findElement(By.xpath("//button[@*='_42ft _4jy0 _al65 _4jy3 _4jy1 selected _51sy']")).click();
+        //3- Ad, soyad, mail ve sifre kutularina deger yazalim ve kaydol tusuna basalim
+        Actions actions = new Actions(driver);
+        WebElement isimKutusu = driver.findElement(By.xpath("//*[@name='firstname']"));
+
+        Faker faker =new Faker();
+        String email =faker.internet().emailAddress();
+
+        actions.click(isimKutusu)
+                .sendKeys(faker.name().firstName())
+                .sendKeys(Keys.TAB)
+                .sendKeys(faker.name().lastName())
+                .sendKeys(Keys.TAB)
+                .sendKeys(email)
+                .sendKeys(Keys.TAB)
+                .sendKeys(email)
+                .sendKeys(Keys.TAB)
+                .sendKeys(faker.internet().password())
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys("15")
+                .sendKeys(Keys.TAB)
+                .sendKeys("Mar")
+                .sendKeys(Keys.TAB)
+                .sendKeys("1990")
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.ARROW_RIGHT)
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.ENTER).perform();
+
+
+        //4- Kaydol tusuna basalim
+        Thread.sleep(5000);
+
+    }
+}
